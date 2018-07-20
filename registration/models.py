841aaac3899bab991aed500919508aa991d95b9f
Christopher Schenk <christopher@cschenk.net>
2018-07-20 22:08:08 +0200

    Added registration models and url file

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from django.db.models import Model, fields, ForeignKey
+from base.models import Clan
+from camps.models import Fee
 
-# Create your models here.
+
+class Registration(Model):
+	clan = ForeignKey(Clan)
+	telephone = fields.CharField(max_length=100)
+	contact_name = fields.CharField(max_length=100)
+	email = fields.EmailField(max_length=100)
+
+
+class TicketInfo(Model):
+	registration = ForeignKey(Registration)
+	fee = ForeignKey(Fee)
+	quantity = fields.PositiveIntegerField()
+
+
+class Ticket(Model):
+	registration = ForeignKey(Registration)
+	fee = ForeignKey(Fee)
+	guid = fields.UUIDField()
+	printed = fields.BooleanField()
+	registrated = fields.DateTimeField(null=True)
