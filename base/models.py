ac6ef3552915fb5d3ebbcff6a3298b9e8ee716d5
Christopher Schenk <christopher@cschenk.net>
2018-07-20 22:06:54 +0200

    Added base app for base data

---
+++ base/models.py
@@ -0,0 +1,11 @@
+from django.db.models import Model, fields, ForeignKey, CASCADE
+class Diocese(Model):
+	name = fields.CharField(max_length=100)
+
+class District(Model):
+	name = fields.CharField(max_length=100)
+	diocese = ForeignKey(Diocese, on_delete=CASCADE)
+
+class Clan(Model)
+	name = fields.CharField(max_length=100)
+	district = ForeignKey(District, on_delete=CASCADE)
