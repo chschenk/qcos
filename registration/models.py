02b12648c3ce3ffa2f8788882e15b8c2b1fa532c
Christopher Schenk <christopher@cschenk.net>
2018-07-21 19:49:41 +0200

    Changed foreignkey relations

--- registration/models.py
+++ registration/models.py
@@ -1,24 +1,26 @@
-from django.db.models import Model, fields, ForeignKey
+from django.db.models import Model, fields, ForeignKey, PROTECT
 from base.models import Clan
 from camps.models import Fee
 
+#TODO: Think about PROTECT/CASCADE for each model
+
 
 class Registration(Model):
-	clan = ForeignKey(Clan)
+	clan = ForeignKey(Clan, on_delete=PROTECT)
 	telephone = fields.CharField(max_length=100)
 	contact_name = fields.CharField(max_length=100)
 	email = fields.EmailField(max_length=100)
 
 
 class TicketInfo(Model):
-	registration = ForeignKey(Registration)
-	fee = ForeignKey(Fee)
+	registration = ForeignKey(Registration, on_delete=PROTECT)
+	fee = ForeignKey(Fee, on_delete=PROTECT)
 	quantity = fields.PositiveIntegerField()
 
 
 class Ticket(Model):
-	registration = ForeignKey(Registration)
-	fee = ForeignKey(Fee)
+	registration = ForeignKey(Registration, on_delete=PROTECT)
+	fee = ForeignKey(Fee, on_delete=PROTECT)
 	guid = fields.UUIDField()
 	printed = fields.BooleanField()
 	registrated = fields.DateTimeField(null=True)
