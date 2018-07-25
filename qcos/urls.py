de7faf511041e21e6faba56fe8234d9310054271
Christopher Schenk <christopher@cschenk.net>
2018-07-25 17:48:01 +0200

    Added URL for api Added namespace for other urls

--- qcos/urls.py
+++ qcos/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	path('camps/', include('camps.urls')),
-	path('registration/', include('registration.urls')),
-	path('base/', include('base.urls')),
+	path('camps/', include('camps.urls', 'camps')),
+	path('registration/', include('registration.urls', 'registration')),
+	path('base/', include('base.urls', 'base')),
+	path('api/', include('api.urls', 'api')),
 ]
