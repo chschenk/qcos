57882fbf9d40e592d4e81f13dc5b93615ef130db
Christopher Schenk <christopher@cschenk.net>
2018-07-29 20:17:34 +0200

    Redircet homepage to registration:list-camps

--- qcos/urls.py
+++ qcos/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('camps/', include('camps.urls')),
+	path('', RedirectView.as_view(pattern_name='camp_registration:list-camps')),
 	path('registration/', include('camp_registration.urls')),
 	path('base/', include('base.urls')),
 	path('api/', include('api.urls')),
